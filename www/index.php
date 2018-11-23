6dae7fc637b314e9663a4c348613d3f30ba53376
Yuzhen Han <yuzhenhanisgood@gmail.com>
2018-11-23 20:28:34 +0000

    Update index.php

--- www/index.php
+++ www/index.php
@@ -17,7 +17,6 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1>testiinggnggg<h1>
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
